59808eb840ea5c10c1ba7655d8a5bd54581064aa
joseph kibithe <joekibithe2@gmail.com>
2025-02-13 00:51:13 +0300

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,7 +73,11 @@ document.querySelectorAll(".add-to-cart-button").forEach((button) => {
         quantity: 1,
       });
     }
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
 
-    console.log(cart);
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
