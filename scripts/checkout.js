a841bff35777ac60b348a1c28e6248e41799e627
joseph kibithe <joekibithe2@gmail.com>
2025-04-11 01:47:23 +0300

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,5 +2,6 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 //import "../data/cart-class.js";
+import "../data/backend-practice.js";
 renderOrderSummary();
 renderPaymentSummary();
